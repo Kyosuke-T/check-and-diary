6ea9dda66e307eb4dde8bc135018c7fb53192d8c
Kyosuke.T <kyosuke@tmgt.co.jp>
2016-03-02 19:55:22 +0900

    first commit

---
+++ Check and Diary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check and Diary
+//
+//  Created by 共田 恭輔 on 2016/02/20.
+//  Copyright © 2016年 共田 恭輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Check and Diary/WriteViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WriteViewController.swift
+//  Check and Diary
+//
+//  Created by 共田 恭輔 on 2016/02/20.
+//  Copyright © 2016年 共田 恭輔. All rights reserved.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
